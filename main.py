418bc85ae35a5ab31d1d9d0eb5e030da9749bac0
Shu Yi Ng <143397026+emxmalene@users.noreply.github.com>
2023-11-27 20:23:11 +0800

    Lab 4

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import sys
-n = len(sys.argv)
 
-a = str(sys.argv[1])
-b = str(sys.argv[2])
-c = str (sys.argv[3])
+# Check if argument are 3 inputs
+if len(sys.argv) != 3:
+    print("Your input is invalid!")
+    sys.exit(1)
 
-if a.isdigit() and b.isdigit() and c.isdigit():
-    add = float(a) + float (b) + float (c)
-    avg = add /3
-    print ("The average of the three numbers is \n {avg:.2f}")
-else:
-    print ("Your input is invalid!")
+try:
